c12e636ae6398e0acd9bb14b98f3f12675eedb5d
owniz <darkoniz@gmail.com>
2018-08-27 22:48:42 +0200

    Added DTO generator for Java

--- dto_gen.py
+++ dto_gen.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import re
 
 
 app = Flask(__name__)
@@ -23,6 +24,40 @@ def generate_dto(indent, tag, pic, name):
 	dto += ('{} WX-DTO{}' + ' '*(14-len(name)) + 'PIC X({:02d}) VALUE SPACES.\n').format(indent, name.upper(), int(pic))
 	dto += ('{} FILLER' + ' '*14 + 'PIC X({:02d}) VALUE \'</{}>\'.\n').format(indent, len(tag)+3, tag.upper())
 	return dto
+
+
+@app.route('/dto_java', methods=['POST'])
+def dto_gen_java():
+	area = request.form['area']
+	return generate_dto_java(area)
+
+
+def lower_one(s):
+	return s[0].lower() + s[1:] if len(s) > 0 else s
+
+
+def generate_dto_java(area):
+	result = ''
+	dto_tags = []
+
+	for line in area.split('\n'):
+		if line.strip():
+			dto_tags.append(re.split(r'[><]', line.strip())[1].upper())
+
+	for i, tag in enumerate(dto_tags):
+		if i == len(dto_tags) - 1:
+			result += '\tprivate String {:s};'.format(tag)
+		else:
+			result += '\tprivate String {:s};\n'.format(tag)
+
+	for tag in dto_tags:
+		result += '\n\n\tpublic String get{:s}() {}\n\t\treturn Utilidades.trim({:s});\n\t{}'.format(tag, unichr(123), tag, unichr(125))
+
+	for tag in dto_tags:
+		result += '\n\n\tpublic set{:s}(String {:s}) {}\n\t\tthis.{:s} =  {:s};\n\t{}'.format(tag, lower_one(tag), unichr(123), tag, tag, unichr(125))
+
+	return result
+
 
 
 if __name__ == '__main__':

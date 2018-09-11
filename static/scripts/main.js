58b728ba69e0c715aca32265d96c207a2be7c44e
owniz <darkoniz@gmail.com>
2018-09-11 20:53:43 +0200

    Now enter works correctly

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,5 +2,6 @@ $(function() {
     $("body")
         .on("click", "#clear_cobol", DtoSender.clearCobol)
         .on("click", "#submit_cobol", DtoSender.submitCobol)
-        .on("click", "#submit_java", DtoSender.submitJava);
+        .on("click", "#submit_java", DtoSender.submitJava)
+        .on("keydown", ".send_enter", function(ev) { if (ev.keyCode === 13) DtoSender.submitCobol(); });
 });
